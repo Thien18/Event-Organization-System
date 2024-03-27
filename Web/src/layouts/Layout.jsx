7776a2ffd9c8669bf5e99a0040640600a0fd9527
Xuan Hoang <xuanh6555@gmail.com>
2024-03-27 12:19:34 +0700

    UI Login,register and home

---
+++ Web/src/layouts/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import Header from '../components/header/Header';
+// import Footer from '../components/footer/Footer';
+
+const Layout = ({children}) => {
+    return (
+        <>
+          
+            {children}
+            
+        </>
+    );
+}
+
+export default Layout;
---
+++ Web/src/pages/index.js
@@ -0,0 +1,3 @@
+export {default as LogIn} from './login/LogIn'
+export {default as Register} from './register/Register'
+export {default as Home} from './home/Home'
